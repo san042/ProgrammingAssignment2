6cb27d7d102c5b8d8a1cd8df029658860271fdc2
san042 <mail2sankumar@gmail.com>
2015-05-20 22:46:51 +0530

    modified by san042 on 20-05-2014

--- cachematrix.R
+++ cachematrix.R
@@ -4,19 +4,22 @@
 ## makeCacheMatrix() function creates a special matrix object which can cache its inverse
 ## which can further be used in cacheSolve() Function.
 
-makeCacheMatrix <- function(x = matrix()) {
-	inverse <- NULL
-    	set <- function(y) {
-        		x <<- y;
-        		inverse <<- NULL;
-    	}
-    	get <- function() return(x);
-   	setinv <- function(inv) inverse <<- inv;
- 	getinv <- function() return(inverse);
-	return(list(set = set, 
-	        get = get, 
-	        setinv = setinv, 
-	        getinv = getinv))
+makeCacheMatrix <- function( m = matrix()) {
+  inv <- NULL  # set inverse as NULL
+  setMatrix <- function(m1) {
+    m <<- m1  # overwrite the existing global variable with the passed matrix
+  }
+  getMatrix <- function() {
+    m  # return the matrix
+  }
+  setSolve <- function(i) {
+    inv <<- i # set the inverse using the passed matrix
+  }
+  getSolve <- function() {
+    inv  # return the invers of a matrix
+  }
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setSolve = setSolve, getSolve = getSolve)
+  # return the list of functions
 }
 
 ## CacheSolve() function return the inverse of the matrix by computing the special matrix 
@@ -24,14 +27,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inverse has already been calculated and the matrix is unchanged then
 ## cacheSolve() functtion will retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        inverse <- x$getinv()
-        if(!is.null(inverse)) {
-                message("Fetching cached data...please wait !!")
-                return(inverse)
-        }
-        data <- x$get()
-        inverse <- solve(data, ...)
-        x$setinv(inverse)
-        return(inverse)
+cacheSolve <- function(x,...) {
+  
+  v <- x$getSolve() # Retrieve any existing inverse
+  if(!is.null(v)) {
+    print("Retrieving cached data ...")
+    return(v)
+  }
+  else {
+    data <- x$getMatrix()
+    if( det(data) == 0) {
+      stop("Since determinant of matrix is 0, Inverse cannot be found")
+    }
+    else {
+      x$setSolve(solve(data)) # set the actual inverse of the given matrix
+    }
+  }
+  v <- solve(data)
+  return(v) 
 }
